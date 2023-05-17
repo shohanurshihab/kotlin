e8f2b3c700f3747348d9375da9010b61e5bc59c7
Shohanur Rahman Shihab <shnrshihab@outlook.com>
2023-05-17 19:11:11 +0600

    variable_update

---
+++ variables/variable_prractice_1.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val totalPhotos=100
+    val deletedPhotos = 10
+    println("$totalPhotos photos")
+    println("$deletedPhotos photos deleted")
+    println("${totalPhotos-deletedPhotos} photos left")
+}
