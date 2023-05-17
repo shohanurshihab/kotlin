a414cfb36bff75ca8d8be066b409449f3ab05843
Shohanur Rahman Shihab <shnrshihab@outlook.com>
2023-05-18 04:42:38 +0600

    boolean

---
+++ variables/boolean.kt
@@ -0,0 +1,5 @@
+fun main() {
+    val meetStatus :Boolean = true
+ 
+    println("You have meetings-> $meetStatus" )
+}
--- variables/string_type.kt
+++ variables/string_type.kt
@@ -1,6 +1,10 @@
 fun main() {
     val nextMeeting = "Next meeting is: "
     val date = "January 1"
+/*  
+    to use quotation->"" inside string values 
+    use escape character -> \ before -> ""
+*/
     val reminder = nextMeeting + date + " at work about \"Kotlin\" "
     println(reminder)
 }
