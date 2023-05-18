85bcec10c56ead42d311fff8fd0deb726292bf4f
shohanurshihab <skshihab531@gmail.com>
2023-05-18 22:48:01 +0600

    argument_pass

---
+++ functions/param.kt
@@ -0,0 +1,8 @@
+fun birthdayGreeting(name: String): String {
+    val nameGreeting = "Happy Birthday, $name!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
+fun main() {
+    println(birthdayGreeting("Rover"))
+}
