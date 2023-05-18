06b296ce8134c6d251bbcc636a3c30704f17279e
shohanurshihab <skshihab531@gmail.com>
2023-05-18 22:58:17 +0600

    multi_arg_pass

--- functions/param.kt
+++ functions/param.kt
@@ -1,8 +1,10 @@
-fun birthdayGreeting(name: String): String {
+fun birthdayGreeting(name: String, age: Int): String {
     val nameGreeting = "Happy Birthday, $name!"
-    val ageGreeting = "You are now 5 years old!"
+    val ageGreeting = "You are now $age years old!"
     return "$nameGreeting\n$ageGreeting"
 }
+
 fun main() {
-    println(birthdayGreeting("Rover"))
+    println(birthdayGreeting("Rover", 5))
+    println(birthdayGreeting("Rex", 2))
 }
