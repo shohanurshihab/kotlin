a9bfea0a9a28952cc62dab44dd472d2d0258eef7
shohanurshihab <skshihab531@gmail.com>
2023-05-18 23:14:32 +0600

    named_args

--- functions/param.kt
+++ functions/param.kt
@@ -1,10 +1,11 @@
-fun birthdayGreeting(name: String, age: Int): String {
+fun birthdayGreeting(name: String = "Rover", age: Int): String {
+    var nk= age + 3;
     val nameGreeting = "Happy Birthday, $name!"
-    val ageGreeting = "You are now $age years old!"
+    val ageGreeting = "You are now $nk years old!"
     return "$nameGreeting\n$ageGreeting"
 }
 
 fun main() {
-    println(birthdayGreeting("Rover", 5))
-    println(birthdayGreeting("Rex", 2))
+    println(birthdayGreeting(age= 5))
+    println(birthdayGreeting(age = 2, name = "Rex"))
 }
