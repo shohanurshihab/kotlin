e4aad88aa3fb8852fbc9f5f8cfb83fadff64f447
shohanurshihab <skshihab531@gmail.com>
2023-05-26 22:45:35 +0600

    condition

---
+++ conditionals/if_else.kt
@@ -0,0 +1,14 @@
+fun main() {
+
+    val trafficLightColor = "Black"
+
+    if (trafficLightColor == "Red") {
+        println("Stop")
+    } else if (trafficLightColor == "Yellow") {
+        println("Slow")
+    } else if (trafficLightColor == "Green") {
+        println("Go")
+    } else {
+        println("Invalid traffic-light color")
+    }
+}
